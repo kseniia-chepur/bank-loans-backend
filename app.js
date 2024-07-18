5f2ca8909b84471207b72f63be5535722b63dd21
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-07-18 09:18:34 +0300

    update server url

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const { serve, setup } = require('swagger-ui-express');
 
 const { loanRouter, clientRouter, loanTypesRouter, authRouter } = require('./routes');
@@ -12,14 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const pathPrefix = 'api/v1';
+app.use('/api-docs', serve, setup(swaggerDocs));
 
-app.use(`/${pathPrefix}/api-docs`, serve, setup(swaggerDocs));
-
-app.use(`/${pathPrefix}/auth`, authRouter);
-app.use(`/${pathPrefix}/clients`, clientRouter);
-app.use(`/${pathPrefix}/loantypes`, loanTypesRouter);
-app.use(`/${pathPrefix}/loans`, loanRouter);
+app.use('/auth', authRouter);
+app.use('/clients', clientRouter);
+app.use('/loantypes', loanTypesRouter);
+app.use('/loans', loanRouter);
 
 app.all('*', (req, res) => {
   res.status(404).json({
