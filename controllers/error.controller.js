17881e1839fb4212a49da20c28b93c6f55144d4e
Kseniia Chepur <kseniia.chepur@gmail.com>
2024-07-19 18:07:50 +0300

    update errorHandler

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -3,5 +3,7 @@ const { httpErrorMsg } = require('../constants');
 exports.globalErrorHandler = (err, req, res, next) => {
   res.status(err.status ?? 500).json({
     message: !err.status || err.status === 500 ? httpErrorMsg.INTERNAL_SERVER_ERROR : err.message,
+    data: err.data,
+    stack: err.stack,
   });
 };
